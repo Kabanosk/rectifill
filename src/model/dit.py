fix: architecture and gradient accumulation
* add proper weight init
* add fix for gradient accumulation for last batch

src/model/dit.py
@@ -52,6 +52,8 @@ class ModulatedLayerNorm(nn.Module):
             nn.SiLU(),
             nn.Linear(condition_dim, 2 * hidden_size)
         )
+        nn.init.zeros_(self.mlp[1].weight)
+        nn.init.zeros_(self.mlp[1].bias)
 
     def forward(self, x: torch.Tensor, condition: torch.Tensor) -> torch.Tensor:
         """
@@ -158,6 +160,7 @@ class DiTModel(BaseModel):
         self.input_proj = nn.Conv1d(in_channels=in_channels, out_channels=config.hidden_size, kernel_size=1)
 
         self.pos_embed = nn.Parameter(torch.zeros(1, config.max_seq_len, config.hidden_size))
+        nn.init.trunc_normal_(self.pos_embed, std=0.02)
 
         self.input_dropout = nn.Dropout(config.dropout)
 
@@ -188,7 +191,7 @@ class DiTModel(BaseModel):
             - text_mask: Optional boolean mask for text embeddings of shape [Batch, Text_Seq_Len].
         :return: Predicted velocity field of shape [Batch, Mel_Bins, Time].
         """
-        t = kwargs.get("t", 0)
+        t = kwargs["t"]
         text_emb = kwargs.get("text_emb", None)
         text_mask = kwargs.get("text_mask", None)
         mel_pad_mask = kwargs.get("mel_pad_mask", None)

src/train.py
@@ -1,12 +1,12 @@
 import argparse
 import dataclasses
+import math
 import random
 import time
 from pathlib import Path
 
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -173,7 +173,7 @@ def main():
 
     scheduler = None
     if optimizer:
-        total_steps = (len(train_loader) // train_config.accumulation_steps) * train_config.epochs
+        total_steps = math.ceil(len(train_loader) / train_config.accumulation_steps) * train_config.epochs
         warmup_scheduler = LinearLR(optimizer, start_factor=0.01, end_factor=1.0, total_iters=train_config.warmup_steps)
         cosine_scheduler = CosineAnnealingLR(optimizer, T_max=(total_steps - train_config.warmup_steps), eta_min=1e-6)
         scheduler = SequentialLR(
@@ -224,7 +224,16 @@ def main():
             )
             loss = F.mse_loss(v_pred, target_v, reduction='none')
             masked_loss = loss[mask_bool.expand_as(loss)].mean()
-            normalized_loss = masked_loss / train_config.accumulation_steps
+
+            is_accumulated = (batch_idx + 1) % train_config.accumulation_steps == 0
+            is_last_batch = (batch_idx + 1) == len(train_loader)
+
+            if is_last_batch and not is_accumulated:
+                effective_accum_steps = len(train_loader) % train_config.accumulation_steps
+            else:
+                effective_accum_steps = train_config.accumulation_steps
+
+            normalized_loss = masked_loss / effective_accum_steps
 
             # --- BACKPROPAGATION ---
             if optimizer: